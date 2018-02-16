bdfa61809a6d6649406bcc4732cef89a2e27071f
linxinzhe <linxinzhechn@icloud.com>
2018-02-16 19:27:42 +0800

    feat(components): components structure

---
+++ src/components/A.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>I'm from A!</p>
+</template>
+
+<script>
+    export default {
+        name: "a"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/App.vue
+++ src/components/App.vue
@@ -1,15 +1,41 @@
 <template>
     <div>
+        <componentA></componentA>
+        <ul>
+            <li v-for="item in list">{{item.name }} - {{item.age}}</li>
+        </ul>
         {{hello}}
+        <button v-on:click="addItem">addItem</button>
     </div>
 </template>
 
 <script>
+    import componentA from "./A.vue"
+
     export default {
         name: "app",
+        components: {
+            componentA: componentA
+        },
         data() {
             return {
-                hello: 'hello test world'
+                hello: 'hello test world',
+                list: [
+                    {name: "linxinzhe", age: 20},
+                    {name: "denglingfeng", age: 22},
+                ],
+                objList: {
+                    name: "laolin",
+                    age: 11,
+                    classroom: "room2"
+                }
+            }
+        },
+        methods: {
+            addItem() {
+                this.list.push({
+                    name: "laoli", age: 44
+                })
             }
         }
     }
